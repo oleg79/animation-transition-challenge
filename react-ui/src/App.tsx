0a88866b31dc2841ca5fa9f24bea599f6eaab33e
Oleg Kapustin <oleg@embeddable.de>
2023-12-14 16:26:49 +0100

    react: button transition to popup. WIP

--- react-ui/src/App.tsx
+++ react-ui/src/App.tsx
@@ -1,12 +1,17 @@
+import React from "react";
 import {EmojiButton} from "./components/EmojiButton.tsx";
 import {CallToActionText} from "./components/CallToActionText.tsx";
 import {Header} from "./components/Header.tsx";
 
 function App() {
+  const [isPopupOpen, setIspOpupOpen] = React.useState<null | boolean>(null);
+
+  const handleTogglePopup = () => setIspOpupOpen(v => !v);
+
   return (
     <>
       <Header/>
-      <EmojiButton emoji="🔥"/>
+      <EmojiButton emoji="🔥" onClick={handleTogglePopup} isPopupOpen={isPopupOpen}/>
       <CallToActionText/>
     </>
   );
--- react-ui/src/components/EmojiButton.tsx
+++ react-ui/src/components/EmojiButton.tsx
@@ -1,10 +1,24 @@
 import React, { ButtonHTMLAttributes } from "react";
 import styles from './EmojiButton.module.css';
 
+
+
 export type EmojiButtonProps = {
   emoji: string;
+  isPopupOpen: boolean | null
 } & ButtonHTMLAttributes<HTMLButtonElement>
-export const EmojiButton: React.FC<EmojiButtonProps> = ({ emoji, ...attributeProps }) =>
-  <button {...attributeProps} className={`${styles.button} pulsate-animation`}>
-    <span>{emoji}</span>
-  </button>;
+export const EmojiButton: React.FC<EmojiButtonProps> = ({ emoji, isPopupOpen, ...attributeProps }) => {
+
+  let anim;
+  if (isPopupOpen === true) anim = styles.transitionToPopup;
+  if (isPopupOpen === false) anim = styles.transitionToButton;
+
+  return (
+    <button
+      {...attributeProps}
+      className={`${styles.button} ${anim}`}
+    >
+      <span>{emoji}</span>
+    </button>
+  );
+}
