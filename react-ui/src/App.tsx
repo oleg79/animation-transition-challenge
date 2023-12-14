1924583751d536dfc0bf81b86a7cbcb974cc0504
Oleg Kapustin <oleg@embeddable.de>
2023-12-14 15:07:24 +0100

    react: add header.

--- react-ui/src/App.tsx
+++ react-ui/src/App.tsx
@@ -1,9 +1,11 @@
 import {EmojiButton} from "./components/EmojiButton.tsx";
 import {CallToActionText} from "./components/CallToActionText.tsx";
+import {Header} from "./components/Header.tsx";
 
 function App() {
   return (
     <>
+      <Header/>
       <EmojiButton emoji="🔥"/>
       <CallToActionText/>
     </>
---
+++ react-ui/src/components/Header.tsx
@@ -0,0 +1,3 @@
+import styles from './Header.module.css';
+
+export const Header = () => <h1 className={styles.header}>Hello{'\n'} there!</h1>;
